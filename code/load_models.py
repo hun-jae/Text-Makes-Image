c322c9d05afe6d33e2e96041d1f95ecb754268a2
hun-jae <61400572+hun-jae@users.noreply.github.com>
2022-05-11 14:02:28 +0900

    Update and rename glide.py to load_models.py

---
+++ code/load_models.py
@@ -0,0 +1,49 @@
+from PIL import Image
+#from IPython.display import display
+import torch as th
+import torch.nn as nn
+
+from glide_text2im.clip.model_creation import create_clip_model
+from glide_text2im.download import load_checkpoint
+from glide_text2im.model_creation import (
+    create_model_and_diffusion,
+    model_and_diffusion_defaults,
+    model_and_diffusion_defaults_upsampler,
+)
+from glide_text2im.tokenizer.simple_tokenizer import SimpleTokenizer
+
+# This notebook supports both CPU and GPU.
+# On CPU, generating one sample may take on the order of 20 minutes.
+# On a GPU, it should be under a minute.
+
+has_cuda = th.cuda.is_available()
+device = th.device('cpu' if not has_cuda else 'cuda')
+
+# Create base model.
+options = model_and_diffusion_defaults()
+options['use_fp16'] = has_cuda
+options['timestep_respacing'] = '100' # use 100 diffusion steps for fast sampling
+model, diffusion = create_model_and_diffusion(**options)
+model.eval()
+if has_cuda:
+    model.convert_to_fp16()
+model.to(device)
+model.load_state_dict(load_checkpoint('base', device))
+print('total base parameters', sum(x.numel() for x in model.parameters()))
+
+# Create upsampler model.
+options_up = model_and_diffusion_defaults_upsampler()
+options_up['use_fp16'] = has_cuda
+options_up['timestep_respacing'] = 'fast27' # use 27 diffusion steps for very fast sampling
+model_up, diffusion_up = create_model_and_diffusion(**options_up)
+model_up.eval()
+if has_cuda:
+    model_up.convert_to_fp16()
+model_up.to(device)
+model_up.load_state_dict(load_checkpoint('upsample', device))
+print('total upsampler parameters', sum(x.numel() for x in model_up.parameters()))
+
+# Create CLIP model.
+clip_model = create_clip_model(device=device)
+clip_model.image_encoder.load_state_dict(load_checkpoint('clip/image-enc', device))
+clip_model.text_encoder.load_state_dict(load_checkpoint('clip/text-enc', device))
