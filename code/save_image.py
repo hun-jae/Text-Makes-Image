86f78b8a29a4b7aa9509ff616d741fb697beccd0
hun-jae <61400572+hun-jae@users.noreply.github.com>
2022-05-11 14:02:01 +0900

    Create save_image.py

---
+++ code/save_image.py
@@ -0,0 +1,96 @@
+def show_images(batch: th.Tensor):
+    """ Display a batch of images inline. """
+    scaled = ((batch + 1)*127.5).round().clamp(0,255).to(th.uint8).cpu()
+    reshaped = scaled.permute(2, 0, 3, 1).reshape([batch.shape[2], -1, 3])
+    img = Image.fromarray(reshaped.numpy())
+    img.save('result.jpg','JPEG')
+    img.show()
+    #display(Image.fromarray(reshaped.numpy()))
+
+# Sampling parameters
+prompt = "an oil painting of a corgi"
+batch_size = 1
+guidance_scale = 3.0
+
+# Tune this parameter to control the sharpness of 256x256 images.
+# A value of 1.0 is sharper, but sometimes results in grainy artifacts.
+upsample_temp = 0.997
+
+##############################
+# Sample from the base model #
+##############################
+
+# Create the text tokens to feed to the model.
+tokens = model.tokenizer.encode(prompt)
+tokens, mask = model.tokenizer.padded_tokens_and_mask(
+    tokens, options['text_ctx']
+)
+
+# Pack the tokens together into model kwargs.
+model_kwargs = dict(
+    tokens=th.tensor([tokens] * batch_size, device=device),
+    mask=th.tensor([mask] * batch_size, dtype=th.bool, device=device),
+)
+
+# Setup guidance function for CLIP model.
+cond_fn = clip_model.cond_fn([prompt] * batch_size, guidance_scale)
+
+# Sample from the base model.
+model.del_cache()
+samples = diffusion.p_sample_loop(
+    model,
+    (batch_size, 3, options["image_size"], options["image_size"]),
+    device=device,
+    clip_denoised=True,
+    progress=True,
+    model_kwargs=model_kwargs,
+    cond_fn=cond_fn,
+)
+model.del_cache()
+
+# Show the output
+
+######show_images(samples)
+
+##############################
+# Upsample the 64x64 samples #
+##############################
+
+tokens = model_up.tokenizer.encode(prompt)
+tokens, mask = model_up.tokenizer.padded_tokens_and_mask(
+    tokens, options_up['text_ctx']
+)
+
+# Create the model conditioning dict.
+model_kwargs = dict(
+    # Low-res image to upsample.
+    low_res=((samples+1)*127.5).round()/127.5 - 1,
+
+    # Text tokens
+    tokens=th.tensor(
+        [tokens] * batch_size, device=device
+    ),
+    mask=th.tensor(
+        [mask] * batch_size,
+        dtype=th.bool,
+        device=device,
+    ),
+)
+
+# Sample from the base model.
+model_up.del_cache()
+up_shape = (batch_size, 3, options_up["image_size"], options_up["image_size"])
+up_samples = diffusion_up.ddim_sample_loop(
+    model_up,
+    up_shape,
+    noise=th.randn(up_shape, device=device) * upsample_temp,
+    device=device,
+    clip_denoised=True,
+    progress=True,
+    model_kwargs=model_kwargs,
+    cond_fn=None,
+)[:batch_size]
+model_up.del_cache()
+
+# Show the output
+show_images(up_samples)
