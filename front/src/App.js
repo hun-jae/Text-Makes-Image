482cb18f9768166b88c694ab748ff3e1958b936e
zero0205 <etety39@naver.com>
2022-10-06 23:44:31 +0900

    Routing setting

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Nav_ from './components/Nav';
+import Login from './pages/Login';
+import { Route, Routes } from 'react-router-dom';
+import MemberSetting from './pages/MemberSetting';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Nav_ />
+      <Routes>
+        <Route path="/" element={"Main page"}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/membersetting" element={<MemberSetting/>}/>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
